443a0b1f4ac38f8b5af701eb479431402ee63c2d
Henrique Riva <hbriva123@gmail.com>
2025-09-13 12:03:34 -0300

    fix: ensure correct redirect on button click

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/recipes'); // ajuste o caminho se necessário
+const routes = require('./routes/recipes');
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
-// Ativa o public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-// Middleware para ler JSON no body
 app.use(express.json());
 
 app.use('/', routes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'views', 'index.html'));
 });
 
-// Start do servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
